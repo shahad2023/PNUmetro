0efcdc0e4e8f6acc698d8baed8687a8e0724b2dd
shahad2023 <shahod.113@hotmail.com>
2022-11-28 17:47:01 +0300

    Initial Commit

---
+++ chal2/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  chal2
+//
+//  Created by Shahad Mohammed on 04/05/1444 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ chal2/chal2App.swift
@@ -0,0 +1,17 @@
+//
+//  chal2App.swift
+//  chal2
+//
+//  Created by Shahad Mohammed on 04/05/1444 AH.
+//
+
+import SwiftUI
+
+@main
+struct chal2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
